973a4b899b96fd92baa76517fe0fe5cb93079a65
Matthew Yeon <m.jiwooyeon@gmail.com>
2022-03-15 12:10:29 -0400

    components updated

---
+++ src/components/video_list/video_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
+
+const VideoList = props => (
+    <ul className={styles.videos}>
+        {props.videos.map(video => (
+            <VideoItem video={video} key={video.id}/>
+        ))}
+    </ul>
+)
+
+export default VideoList;
