742936dbc0c7cf1dc8b03629ce7b17ffc998f579
Matthew Yeon <m.jiwooyeon@gmail.com>
2022-03-19 00:30:05 -0400

    video detail added

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import VideoItem from '../video_item/video_item';
 import styles from './video_list.module.css';
 
-const VideoList = props => (
+const VideoList = ({videos, onVideoClick, display}) => (
     <ul className={styles.videos}>
-        {props.videos.map(video => (
-            <VideoItem video={video} key={video.id}/>
+        {videos.map(video => (
+            <VideoItem 
+                key={video.id}
+                video={video}
+                onVideoClick={onVideoClick}
+                display={display}
+            />
         ))}
     </ul>
 )
-
 export default VideoList;
